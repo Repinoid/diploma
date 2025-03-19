32736aec2e877e46409620b8faa26e24d058ebcb
Naeel <tazet@narod.ru>
2025-03-19 20:31:43 +0400

    no tx

--- internal/securitate/tableFuncs.go
+++ internal/securitate/tableFuncs.go
@@ -143,11 +143,11 @@ func (dataBase *DBstruct) AccuOrders(ctx context.Context) (err error) {
 			return err
 		}
 
-		tx, err := db.Begin(ctx)
-		if err != nil {
-			models.Sugar.Debugf("error db.Begin  %[1]w", err)
-		}
-		defer tx.Rollback(ctx)
+		// tx, err := db.Begin(ctx)
+		// if err != nil {
+		// 	models.Sugar.Debugf("error db.Begin  %[1]w", err)
+		// }
+		// defer tx.Rollback(ctx)
 
 		for _, ord := range orda {
 			accuOrderStat, status, err := rual.GetFromAccrual(ord.Number)
@@ -156,20 +156,18 @@ func (dataBase *DBstruct) AccuOrders(ctx context.Context) (err error) {
 				continue
 			}
 			updateOrder := "UPDATE orders SET orderStatus = $2, accrual = $3 WHERE orderNumber  = $1 ;"
-			_, err = tx.Exec(ctx, updateOrder, ord.Number, accuOrderStat.Status, accuOrderStat.Accrual)
+			_, err = db.Exec(ctx, updateOrder, ord.Number, accuOrderStat.Status, accuOrderStat.Accrual)
 			if err != nil {
 				models.Sugar.Debugf("error exex %v", err)
 				return err
 			}
 
 		}
-		err = tx.Commit(ctx)
-		if err != nil {
-			//		status = http.StatusInternalServerError //500 — внутренняя ошибка сервера.
-			models.Sugar.Debugf(" tx.Commit %+v\n", err)
-		}
+		// err = tx.Commit(ctx)
+		// if err != nil {
+		// 	//		status = http.StatusInternalServerError //500 — внутренняя ошибка сервера.
+		// 	models.Sugar.Debugf(" tx.Commit %+v\n", err)
+		// }
 		time.Sleep(time.Second) // секунда задержки ... а сколько надо ставить ? или надо запускать после/перед каждым http обращением к таблице заказов ? триггеря через канал, например
 	}
-	//	status = http.StatusOK
-	//	return
 }
